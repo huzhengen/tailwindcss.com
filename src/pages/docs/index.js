6916213a8dc113d95abf8083c182f2f46a9589d6
hukai <huzhengen@163.com>
2021-01-18 17:23:58 +0800

    docs: translate pages/docs/index.js

--- src/pages/docs/index.js
+++ src/pages/docs/index.js
@@ -98,23 +98,23 @@ export default function DocsLandingPage() {
   return (
     <div className="px-4 sm:px-6 xl:px-8 pt-10 pb-16">
       <h1 className="text-5xl leading-none font-extrabold text-gray-900 tracking-tight mb-4">
-        Getting started with Tailwind CSS
+        Tailwind CSS 入门
       </h1>
       <p className="text-2xl tracking-tight mb-10">
-        Learn Tailwind the way that best matches your learning style.
+        用最适合你的方式学习 Tailwind。
       </p>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 xl:gap-8">
         <section className="flex">
           <div className="w-full relative text-white overflow-hidden rounded-3xl flex shadow-lg">
             <div className={`w-full flex md:flex-col bg-gradient-to-br ${gradients.violet[0]}`}>
               <div className="sm:max-w-sm sm:flex-none md:w-auto md:flex-auto flex flex-col items-start relative z-10 p-6 xl:p-8">
-                <h2 className="text-xl font-semibold mb-2 text-shadow">Read the docs</h2>
+                <h2 className="text-xl font-semibold mb-2 text-shadow">阅读文档</h2>
                 <p className="font-medium text-violet-100 text-shadow mb-4">
-                  Learn how to get Tailwind set up in your project.
+                  学习如何在项目中设置 Tailwind。
                 </p>
                 <Link href="/docs/installation">
                   <a className="mt-auto bg-violet-800 bg-opacity-50 hover:bg-opacity-75 transition-colors duration-200 rounded-xl font-semibold py-2 px-4 inline-flex">
-                    Start learning
+                    开始学习
                   </a>
                 </Link>
               </div>
@@ -134,15 +134,15 @@ export default function DocsLandingPage() {
           <div className="w-full relative text-white overflow-hidden rounded-3xl flex shadow-lg">
             <div className={`w-full flex md:flex-col bg-gradient-to-br ${gradients.pink[0]}`}>
               <div className="sm:max-w-sm sm:flex-none md:w-auto md:flex-auto flex flex-col items-start relative z-10 p-6 xl:p-8">
-                <h2 className="text-xl font-semibold mb-2 text-shadow">Try it in the browser</h2>
+                <h2 className="text-xl font-semibold mb-2 text-shadow">在浏览器中尝试</h2>
                 <p className="font-medium text-rose-100 text-shadow mb-4">
-                  Build something with Tailwind in our online playground.
+                  在我们的在线环境中构建。
                 </p>
                 <a
                   href="https://play.tailwindcss.com/"
                   className="mt-auto bg-rose-900 bg-opacity-50 hover:bg-opacity-75 transition-colors duration-200 rounded-xl font-semibold py-2 px-4 inline-flex"
                 >
-                  Start building
+                  开始构建
                 </a>
               </div>
               <div className={`${styles.image} relative md:pl-6 xl:pl-8 hidden sm:block`}>
@@ -156,13 +156,13 @@ export default function DocsLandingPage() {
           <div className="w-full relative text-white overflow-hidden rounded-3xl flex shadow-lg">
             <div className={`w-full flex md:flex-col bg-gradient-to-br ${gradients.amber[0]}`}>
               <div className="sm:max-w-sm sm:flex-none md:w-auto md:flex-auto flex flex-col items-start relative z-10 p-6 xl:p-8">
-                <h2 className="text-xl font-semibold mb-2 text-shadow">Watch the screencasts</h2>
+                <h2 className="text-xl font-semibold mb-2 text-shadow">观看视频</h2>
                 <p className="font-medium text-amber-100 text-shadow mb-4">
-                  Learn more about Tailwind directly from the team on our channel.
+                  通过我们的频道直接从团队学习 Tailwind。
                 </p>
                 <Link href="https://www.youtube.com/tailwindlabs">
                   <a className="mt-auto bg-amber-900 bg-opacity-50 hover:bg-opacity-75 transition-colors duration-200 rounded-xl font-semibold py-2 px-4 inline-flex">
-                    Start watching
+                    开始观看
                   </a>
                 </Link>
               </div>
@@ -181,19 +181,19 @@ export default function DocsLandingPage() {
             <TuiLogo className="w-40 h-auto" />
           </h2>
           <p className="flex-auto text-white text-lg font-medium">
-            <Widont>Beautiful UI components, crafted by the creators of Tailwind CSS</Widont>
+            <Widont>由 Tailwind CSS 创建者精心打造的精美 UI 组件</Widont>
           </p>
           <a
             href="https://tailwindui.com/components"
             className="flex-none bg-white hover:bg-gray-100 transition-colors duration-200 text-gray-900 font-semibold rounded-lg py-3 px-4"
           >
-            Browse components
+            浏览组件
           </a>
         </section>
       </div>
       <section>
         <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 mt-16 mb-8">
-          What’s new in Tailwind
+          Tailwind 的新功能
         </h2>
         <ul className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6 xl:gap-8 font-semibold text-gray-900 text-center">
           {whatsNew.map((item) => (
@@ -215,9 +215,9 @@ export default function DocsLandingPage() {
       </section>
       <section>
         <header className="flex items-center justify-between mt-16 mb-8">
-          <h2 className="text-3xl tracking-tight font-extrabold text-gray-900">Latest Updates</h2>
+          <h2 className="text-3xl tracking-tight font-extrabold text-gray-900">最新更新</h2>
           <a href="https://blog.tailwindcss.com" className="font-medium text-gray-900">
-            View all the latest updates
+            查看所有的最新更新
           </a>
         </header>
         <ul className="bg-gray-50 rounded-3xl p-2 sm:p-5 xl:p-6">
@@ -285,7 +285,7 @@ export default function DocsLandingPage() {
       </section>
       <section>
         <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 mt-16 mb-8">
-          Get involved
+          参与我们
         </h2>
         <Community />
       </section>
